9eca36efa15f0fbfd476aa620463e4fe8780f23f
Ulwothar <mateuszkrp@gmail.com>
2022-03-16 20:33:23 +0000

    Enabled cors for all origins.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.setGlobalPrefix('api');
   await app.listen(4002);
 }
